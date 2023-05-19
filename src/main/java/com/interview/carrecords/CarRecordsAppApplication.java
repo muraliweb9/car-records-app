b0b931deefd538c3205e5c9f6cce934dbf8dd8e3
muraliweb9 <muraliweb@hotmail.com>
2023-05-19 22:03:08 +0100

    Initial

---
+++ src/main/java/com/interview/carrecords/CarRecordsAppApplication.java
@@ -0,0 +1,13 @@
+package com.interview.carrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRecordsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRecordsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/test/carrecords/CarRecordsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.test.carrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRecordsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
