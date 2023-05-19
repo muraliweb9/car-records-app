cdc4142084cb22d367724e46e4fbc5280e7904f9
muraliweb9 <muraliweb@hotmail.com>
2023-05-19 23:06:13 +0100

    Initial

--- src/test/java/com/interview/carrecords/CarRecordsAppApplicationTests.java
+++ src/test/java/com/interview/carrecords/CarRecordsAppApplicationTests.java
@@ -1,15 +1,11 @@
-package com.interview.test.carrecords;
+package com.interview.carrecords;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarRecordsAppApplicationTests {
 
-	@Test
-	@Disabled
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
