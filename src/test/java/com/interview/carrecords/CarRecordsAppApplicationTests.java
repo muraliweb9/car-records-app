61f5f4ce2084b9d6f4b24d5690feefa7b5da2484
muraliweb9 <muraliweb@hotmail.com>
2023-05-24 15:56:30 +0100

    Initial

--- src/test/java/com/interview/carrecords/CarRecordsAppApplicationTests.java
+++ src/test/java/com/interview/carrecords/CarRecordsAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.interview.carrecords;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CarRecordsAppApplicationTests {
 
     @Test
